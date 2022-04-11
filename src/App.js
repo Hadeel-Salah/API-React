4e3d119b9cbec70a86dc42a1a1a42f8d2d99e1e7
hadeelsala7 <h.salah.technical@gmail.com>
2022-04-11 16:41:23 +0300

    push file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/app.css';
+import { LogoIcon } from "./assets/icons"
+import CrudUser from "./components/CrudUser"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <div className='header__content'>
+          <div className='logo'>
+            <LogoIcon />
+            <strong>JSON SERVER API</strong>
+          </div>
+        </div>
       </header>
-    </div>
+      <main>
+        <CrudUser />
+      </main>
+    </>
   );
 }
 
